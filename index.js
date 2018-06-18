1c0386e18f8ab27aab5c4183d3c415070d92d139
shekharsingh1987 <shekharsingh_1987@yahoo.com>
2018-06-18 17:24:38 +0530

    Adding the port change to 8080 and added process.env

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.post("/subscribe", (req, res) => {
     .catch(err => console.error(err));
 });
 
-const port = 8080;
+// const port = 8080;
+var port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
